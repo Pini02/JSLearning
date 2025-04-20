ebdcae5045909fc7af7b64fcffb15a04b5cb8766
Pini02 <marcopini87@gmail.com>
2025-04-20 11:03:52 +0200

    Functions 2

--- functions2.js
+++ functions2.js
@@ -1,14 +1,25 @@
 //Estrazione componenti richieste.
-const ciaoButton = document.querySelector('button')
-const parag = document.querySelector('p');
-const input = document.querySelector('input')
+const ciaoButton = document.querySelector('#nomeb',)
+const paragc = document.querySelector('#nomep');
+const inputc = document.querySelector('#nomei')
 
+const cubob = document.querySelector('#cubob')
+const cubop = document.querySelector('#cubop')
+const cuboi = document.querySelector('#cuboi')
 //Funzione che modifica il valore del componente p usando la stringa in accenti gravi.
 function hello(name){
-    parag.innerHTML=`Ciao ${name}`
+    paragc.innerHTML=`Ciao ${name}`
+}
+
+function cubo(value){
+    return value*value*value
 }
 
 //Aggiunta di event listener per evento click in bottone.
 ciaoButton.addEventListener('click', ()=> {
-    hello(input.value)
+    hello(inputc.value)
+})
+
+cubob.addEventListener('click', () =>{
+    cubop.innerHTML=cubo(cuboi.value)
 })
